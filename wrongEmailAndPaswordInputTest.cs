da919c0643552da55d9c8f665d096935fb1b807b
arturasZap <112495317+arturasZap@users.noreply.github.com>
2022-09-06 17:09:27 +0300

    Add files via upload

---
+++ wrongEmailAndPaswordInputTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace Baigiamasis_Projektas.Tests
+{
+    internal class wrongEmailAndPaswordInputTest : BaseTest
+    {
+        [Test]
+
+        public static void TestingWrongLoginCredits()
+
+        {
+            string email = "vcs@mokausi.lt";
+            string password = "astuonkojis";
+
+            _ = _wrongLoginPage.NavigateToDefaultPage();
+            _ = _wrongLoginPage.AgePopUpAgree();
+            _ = _wrongLoginPage.LoginLogoClick();
+            _ = _wrongLoginPage.EmailInputField(email);
+            _ = _wrongLoginPage.PaswordInputField(password);
+            _ = _wrongLoginPage.ClickSubmitButton();
+            _ = _wrongLoginPage.LoginErrorShown();
+        }
+    }
+
+}
